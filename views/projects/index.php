0abded39d04cee7cb51189d61f606ef298233961
LACOMBE Dominique <lacombe.dominique@outlook.fr>
2018-05-22 19:32:55 -0400

    Mise en page de la liste des projets

--- views/projects/index.php
+++ views/projects/index.php
@@ -12,20 +12,13 @@ foreach ($viewModel as $project)
         <h2><?php echo $project['framework']; ?></h2>
         <?php
     }
-    $uniqueId = $project['id'].$project['title'];
     ?>
-
-    <!-- DEBUT Partie à mettre à jour -->
-    <div class="project-summary">
-        <form method="post">
-            <button class="project-btn" type="submit" formaction="<?php echo ROOT_URL.'project/display/'.$project['id']; ?>">
-                <img src="data:image/jpeg;base64,<?php echo $project['img_blob']; ?>" alt="<?php echo $project['title']; ?>">
-                <h4><?php echo $project['title']; ?></h4>
-            </button>
-        </form>
-    </div>
-    <!-- FIN Partie à mettre à jour -->
-
+    <form method="post">
+        <button class="project-btn" type="submit" formaction="<?php echo ROOT_URL.'project/display/'.$project['id']; ?>">
+            <img src="data:image/jpeg;base64,<?php echo $project['img_blob']; ?>" alt="<?php echo $project['title']; ?>">
+            <h4><?php echo $project['title']; ?></h4>
+        </button>
+    </form>
     <?php
     $oldFramework = $project['framework'];
 }
