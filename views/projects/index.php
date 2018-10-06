6b18b03245b4441d55ff32bbd820ef4414506beb
fenfen <liufen2004@hotmail.com>
2018-10-06 09:09:12 -0400

    Modification Liste des projets
    Disposition sous forme de tuiles
    Ajout d'une description qui s'affiche lors du passage de la souris

--- views/projects/index.php
+++ views/projects/index.php
@@ -2,25 +2,15 @@
 $oldFramework = '';
 foreach ($viewModel as $project)
 {
-    if ($oldFramework != urldecode($project['framework']))
-    {
-        // Fermeture du DIV framework-summary après changement de framework (sauf le premier)
-        if ($oldFramework != '') { ?> </div> <?php }
-
-        // Ouverture du DIV framework-summary ?>
-        <div class="framework-summary">
-        <h2><?= urldecode($project['framework']); ?></h2>
-        <?php
-    }
     ?>
-    <form method="post">
-        <button class="project-btn" type="submit" formaction="<?= ROOT_URL.'project/display/'.$project['id']; ?>">
-            <img src="data:image/jpeg;base64,<?= $project['img_blob']; ?>" alt="<?= urldecode($project['title']); ?>">
-            <h4><?= urldecode($project['title']); ?></h4>
-        </button>
-    </form>
+    <div class="project">
+        <img class="project-image" src="data:image/jpeg;base64,<?= $project['img_blob']; ?>">
+        <div class="project-overlay">
+            <div class="project-desc">
+                <p><?= urldecode($project['title']); ?></p>
+            </div>
+        </div>
+    </div>
     <?php
-    $oldFramework = urldecode($project['framework']);
 }
 ?>
-</div> <!-- Fermeture du dernier DIV framework-summary -->
