44b3a80784778b5f02b552462bbb28b3c8319f80
fenfen <liufen2004@hotmail.com>
2018-10-06 11:38:06 -0400

    Modification Liste des projets
    Ajout du lien vers la description complète du projet

--- views/projects/index.php
+++ views/projects/index.php
@@ -3,7 +3,7 @@ $oldFramework = '';
 foreach ($viewModel as $project)
 {
     ?>
-    <div class="project">
+    <a class="project" href="<?= ROOT_URL.'project/display/'.$project['id']; ?>">
         <img class="project-image" src="data:image/jpeg;base64,<?= $project['img_blob']; ?>">
         <div class="project-overlay">
             <div class="project-desc">
@@ -11,7 +11,7 @@ foreach ($viewModel as $project)
                 <p><?= urldecode($project['desc_'.strtolower($_SESSION['language'])]); ?></p>
             </div>
         </div>
-    </div>
+    </a>
     <?php
 }
 ?>
