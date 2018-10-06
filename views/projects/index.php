f520d8fc95d137a34d018734ad8e793f74aa3ae8
fenfen <liufen2004@hotmail.com>
2018-10-06 12:25:42 -0400

    Liste des projets
    Ajout d'un background
    Modification de l'opacité

--- views/projects/index.php
+++ views/projects/index.php
@@ -3,10 +3,11 @@ $oldFramework = '';
 foreach ($viewModel as $project)
 {
     ?>
-    <a class="project" href="<?= ROOT_URL.'project/display/'.$project['id']; ?>">
-        <img class="project-image" src="data:image/jpeg;base64,<?= $project['img_blob']; ?>">
-        <div class="project-overlay">
-            <div class="project-desc">
+    <a class="project-list" href="<?= ROOT_URL.'project/display/'.$project['id']; ?>">
+        <img class="project-list-image" src="data:image/jpeg;base64,<?= $project['img_blob']; ?>">
+        <div class="project-list-overlay">
+            <div class="project-list-overlay-back"></div>
+            <div class="project-list-desc">
                 <h4><?= urldecode($project['title']); ?></h4>
                 <p><?= urldecode($project['desc_'.strtolower($_SESSION['language'])]); ?></p>
             </div>
